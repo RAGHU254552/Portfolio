021f93f5c820ffe30f41f1d8c3d6b438879aedc3
Raghuveer Singh <rajputraghu254@gmail.com>
2025-12-02 14:48:55 +0530

    Resume Updated

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -28,7 +28,11 @@ export const AboutSection = () => {
               </a>
 
               <a
+<<<<<<< HEAD
                 href="/projects/Raghuveer(Resume).pdf"
+=======
+                href="/projects/Raghuveer Singh.pdf"
+>>>>>>> 2669fe6 (Resume Update)
                 download
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/15 transition-colors duration-300"
               >
